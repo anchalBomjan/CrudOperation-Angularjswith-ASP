a6cf529342c5407291a0a13f589ce61cbb13cf3d
anchalBomjan <anchal.percoidit@gmail.com>
2024-10-18 14:27:48 +0545

    Adding Category Feature using static data

--- Client/app/app.js
+++ Client/app/app.js
@@ -3,16 +3,20 @@ var app = angular.module('productApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/products', {
-            templateUrl: 'views/products.html',  // Adjusted path
+            templateUrl: 'Views/products.html',  // Adjusted path
             controller: 'ProductController'
         })
         .when('/add-product', {
-            templateUrl: 'views/addProduct.html',  // Adjusted path
+            templateUrl: 'Views/addProduct.html',  // Adjusted path
             controller: 'ProductController'
         })
         .when('/edit/:id',{
-            templateUrl:'views/Edit.html',
+            templateUrl:'Views/Edit.html',
             controller:'ProductController'
+        })
+        .when('/categories', {
+            templateUrl: 'Views/category.html', // Correct path
+            controller: 'CategoryController'
         })
         .otherwise({
             redirectTo: '/products'
