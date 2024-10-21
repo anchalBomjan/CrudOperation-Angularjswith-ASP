efb44add4685179a505d3b4081ffc6c0d49aa47d
anchalBomjan <anchal.percoidit@gmail.com>
2024-10-21 10:31:44 +0545

    some changes on Client

--- Client/app/app.js
+++ Client/app/app.js
@@ -3,19 +3,19 @@ var app = angular.module('productApp', ['ngRoute']);
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/products', {
-            templateUrl: 'Views/products.html',  // Adjusted path
+            templateUrl: 'views/products.html',  // Adjusted path
             controller: 'ProductController'
         })
         .when('/add-product', {
-            templateUrl: 'Views/addProduct.html',  // Adjusted path
+            templateUrl: 'views/addProduct.html',  // Adjusted path
             controller: 'ProductController'
         })
         .when('/edit/:id',{
-            templateUrl:'Views/Edit.html',
+            templateUrl:'views/Edit.html',
             controller:'ProductController'
         })
         .when('/categories', {
-            templateUrl: 'Views/category.html', // Correct path
+            templateUrl: 'views/category.html', // Correct path
             controller: 'CategoryController'
         })
         .otherwise({
